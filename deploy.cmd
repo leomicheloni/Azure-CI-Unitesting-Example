387bc7c68b811e92c460b9e86e6f7040755f5d84
Leonardo <leomicheloni@hotmail.com>
2015-10-05 19:49:55 -0300

    .

--- deploy.cmd
+++ deploy.cmd
@@ -1,9 +1,9 @@
 :: 1. building
-%MSBUILD_PATH% "%DEPLOYMENT_SOURCE%\WebApp1\WebApp1.csproj"
+"%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\WebApp1\WebApp1.csproj"
 IF !ERRORLEVEL! NEQ 0 goto error
 
 :: 2. building tests
-%MSBUILD_PATH% "%DEPLOYMENT_SOURCE%\WebApp1.Tests\WebApp1.Tests.csproj"
+"%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\WebApp1.Tests\WebApp1.Tests.csproj"
 IF !ERRORLEVEL! NEQ 0 goto error
 
 :: 3. Testing
