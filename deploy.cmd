5b02f95a726473d8637ecfb882bad99d2719e77a
Leonardo <leomicheloni@hotmail.com>
2015-10-05 19:45:22 -0300

    fix

--- deploy.cmd
+++ deploy.cmd
@@ -1,8 +1,12 @@
-echo :: 2. Tests
-echo 2: Build and execute tests
-echo 2a: Executing Unit Tests
+:: 1. building
 
-%MSBUILD_PATH% "%DEPLOYMENT_SOURCE%\WebApp1.Tests\WebApp1.Tests.csproj" /nologo /verbosity:m /t:Build /p:Configuration=Debug
+%MSBUILD_PATH% "%DEPLOYMENT_SOURCE%\WebApp1\WebApp1.csproj" /nologo /verbosity:d /t:Build /p:Configuration=Debug
+
+:: 2. building tests
+
+%MSBUILD_PATH% "%DEPLOYMENT_SOURCE%\WebApp1.Tests\WebApp1.Tests.csproj" /nologo /verbosity:d /t:Build /p:Configuration=Debug
+
+:: 3. Testing
 vstest.console.exe "%DEPLOYMENT_SOURCE%\WebApp1.Tests\bin\Debug\WebApp1.Tests.dll"
 
 IF !ERRORLEVEL! NEQ 0 goto error
