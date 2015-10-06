99565f5087cc1987de8531eab40d0e281229ae38
Leonardo <leomicheloni@hotmail.com>
2015-10-05 22:26:04 -0300

    bat fix

--- deploy.cmd
+++ deploy.cmd
@@ -1,7 +1,9 @@
-REM Compilamos el proyecto web
+::1. Compilamos el proyecto web
 "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\WebApp1\WebApp1\WebApp1.csproj"
-REM Compilamos el proyecto de test
+
+::2. Compilamos el proyecto de test
 "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\WebApp1\WebApp1.Tests\WebApp1.Tests.csproj"
-REM ejecutamos los test
+
+::3. Ejecutamos los test
 vstest.console.exe "%DEPLOYMENT_SOURCE%\WebApp1\WebApp1.Tests\bin\Debug\WebApp1.Tests.dll"
 
