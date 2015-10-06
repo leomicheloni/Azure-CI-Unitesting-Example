be6e45f25930a1461ca5df021819c4c538b2f247
Leonardo <leomicheloni@hotmail.com>
2015-10-05 22:20:01 -0300

    comments added

--- deploy.cmd
+++ deploy.cmd
@@ -1,8 +1,7 @@
-
+REM Compilamos el proyecto web
 "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\WebApp1\WebApp1\WebApp1.csproj"
-
-
+REM Compilamos el proyecto de test
 "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\WebApp1\WebApp1.Tests\WebApp1.Tests.csproj"
-
+REM ejecutamos los test
 vstest.console.exe "%DEPLOYMENT_SOURCE%\WebApp1\WebApp1.Tests\bin\Debug\WebApp1.Tests.dll"
 
