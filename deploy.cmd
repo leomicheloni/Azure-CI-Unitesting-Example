5d556912dfe513c698417e7f8d0b59ef1dcf936b
Leonardo <leomicheloni@hotmail.com>
2015-10-05 22:42:02 -0300

    .

--- deploy.cmd
+++ deploy.cmd
@@ -1,9 +1,7 @@
-::1. Compilamos el proyecto web
+REM Compilamos el proyecto web
 "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\WebApp1\WebApp1\WebApp1.csproj"
-
-::2. Compilamos el proyecto de test
+REM Compilamos el proyecto de test
 "%MSBUILD_PATH%" "%DEPLOYMENT_SOURCE%\WebApp1\WebApp1.Tests\WebApp1.Tests.csproj"
-
-::3. Ejecutamos los test
+REM ejecutamos los test
 vstest.console.exe "%DEPLOYMENT_SOURCE%\WebApp1\WebApp1.Tests\bin\Debug\WebApp1.Tests.dll"
 
